071d7d93ff3277af1f2e786a9d56464ddb78e2c5
Sonu Kumar Shah <91136335+Always4ukittu@users.noreply.github.com>
2023-08-07 20:09:16 +0530

    Update views.py

--- project8/CodeMaverick/views.py
+++ project8/CodeMaverick/views.py
@@ -11,57 +11,69 @@ from django.urls import reverse
 from datetime import datetime
 from .models import *
 
+
 def home(request):
     return render(request, "CodeMaverick/index.html")
+
 
 def load_profile(request):
     return render(request, "CodeMaverick/profile.html")
 
+
 def teams(request):
     return render(request, "CodeMaverick/teams.html")
 
+
 def load_teams(request):
     try:
-        my_teams = Team.objects.filter(members = request.user)
-        teams = Team.objects.exclude(members = request.user).order_by("rank").all()
-        return JsonResponse({"my_teams":[my_team.team_info() for my_team in my_teams], "teams":[team.team_info() for team in teams]}, safe = False, status = 201)
+        my_teams = Team.objects.filter(admin_id=request.user)
+        teams = Team.objects.exclude(admin_id=request.user).order_by("rank").all()
+        return JsonResponse({"my_teams": [my_team.team_info() for my_team in my_teams], "teams": [team.team_info() for team in teams]}, safe=False, status=201)
     except:
-        return JsonResponse({"error":"Something went wrong! Cannot load teams!"}, status = 200)
+        return JsonResponse({"error": "Something went wrong! Cannot load teams!"}, status=200)
 
+
+@csrf_exempt
 def create_new_team(request):
-    data = json.loads(request.body)
-    new_team_name = data.get("data")
-    try:
-        if not new_team_name.isalnum() or 3 > len(new_team_name) > 24:
-            raise ValueError
-        temp_team = Team.objects.filter(name = new_team_name)
-        if temp_team:
-            raise IntegrityError
-        new_team_id = get_random_string(length=12)
-        while(True):
-            temp_team = Team.objects.filter(id = new_team_id)
-            if not temp_team:
-                break
+    if request.method == "POST":
+        data = json.loads(request.body)
+        new_team_name = data.get("data")
+        try:
+            if not new_team_name.isalnum() or 3 > len(new_team_name) > 24:
+                raise ValueError
+            temp_team = Team.objects.filter(name=new_team_name)
+            if temp_team:
+                raise IntegrityError
             new_team_id = get_random_string(length=12)
-        new_rank = len(Team.objects.all())
-        new_team = Team.objects.create(id = new_team_id, name = new_team_name, admin = request.user, rank = new_rank)
-        new_team.members.add(request.user)
-        new_team.save()
-        return JsonResponse({"success": "Team created succesfully!"})
-    except IntegrityError:
-        return JsonResponse({"error":"Team name not available!"})
-    except ValueError:
-        return JsonResponse({"error":"Team name cannot have characters other than a-z, A-Z, 0-9, _; length must be greter than 3 and less than/equal 24"})
-    except:
-        return JsonResponse({"error":"Something went wrong!"})
+            print(new_team_id, "$$$$$$$$$$$$$$$$$$$$")  # kjsfkldflk klfdslk
+            while (True):
+                temp_team = Team.objects.filter(team_id=new_team_id)
+                print(temp_team, "$$$$$$$$$$$$$$$$$$$$")
+                if not temp_team:
+                    break
+                new_team_id = get_random_string(length=12)
+            new_rank = len(Team.objects.all())
+            new_team = Team.objects.create(team_id=new_team_id, name=new_team_name, admin=request.user, rank=new_rank)
+            new_team.members = request.user
+            new_team.save()
+            return JsonResponse({"success": "Team created succesfully!"})
+        except IntegrityError:
+            return JsonResponse({"error": "Team name not available!"})
+        except ValueError:
+            return JsonResponse({"error": "Team name cannot have characters other than a-z, A-Z, 0-9, _; length must be greter than 3 and less than/equal 24"})
+        except:
+            return JsonResponse({"error": "Something went wrong!"})
+
 
 def apply_team(request, team_id):
     data = request.json.loads("body")
     message = data["message"]
     notif_id = len(Notification.objects.all)
-    notif = Notification(id = notif_id, sender = request.user, receiver =  Team.objects.get(team_id = team_id).admin.username, message = message)
+    notif = Notification(id=notif_id, sender=request.user, receiver=Team.objects.get(
+        team_id=team_id).admin.username, message=message)
     notif.save()
-    
+
+
 @csrf_exempt
 def login_view(request):
     if request.method == "POST":
@@ -81,7 +93,6 @@ def login_view(request):
         return render(request, "CodeMaverick/login.html")
 
 # def login_API_google():
-
 
 
 def logout_view(request):
@@ -115,4 +126,3 @@ def register(request):
         return HttpResponseRedirect(reverse("home"))
     else:
         return render(request, "CodeMaverick/register.html")
-    
